e214402877cb8bd8e43fb8417fa4863bb1814626
Aadit M Shah <aaditmshah@fastmail.fm>
2019-08-01 23:01:01 +0530

    load the app as quickly as possible

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-workbox.routing.registerRoute(/\.js$/, new workbox.strategies.NetworkFirst({
+workbox.routing.registerRoute(/\.js$/, new workbox.strategies.StaleWhileRevalidate({
     cacheName: "js-cache"
 }));
 
